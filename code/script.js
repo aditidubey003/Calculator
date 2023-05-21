02bcfc93873a3e50de241311aa8f3410f57f65f9
Aditi Dubey <aditidubey576@gmail.com>
2023-05-21 09:39:02 +0530

    made it responsive for Mobile screen

---
+++ code/script.js
@@ -0,0 +1,19 @@
+
+// Giving the Functionality 
+
+let root = document.querySelector('.calculation')
+root.addEventListener("click", function(event){
+// console.log(event.target)
+// console.dir(event.target)
+let operator = event.target.dataset.text
+
+})
+
+
+
+/*function sum (num, num){
+let result =  num + num;
+let output = document.querySelector('.output')
+return output.innerText = result;
+}
+*/
